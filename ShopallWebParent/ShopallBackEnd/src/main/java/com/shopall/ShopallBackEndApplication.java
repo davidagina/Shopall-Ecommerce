5b60b2663992080af45a695f741439254e4b1670
davidagina <davidagina64@gmail.com>
2023-11-10 07:14:01 +0100

    Added ShopallBackEnd

---
+++ ShopallWebParent/ShopallBackEnd/src/main/java/com/shopall/ShopallBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.shopall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopallBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopallBackEndApplication.class, args);
+	}
+
+}
