7c7c0a72cb7a6b767245d89a840a6cc34388cef7
davidagina <davidagina64@gmail.com>
2023-11-11 10:53:43 +0100

    Added ShopallFrontEnd

---
+++ ShopallWebParent/ShopallFrontEnd/src/main/java/com/shopall/web/ShopallFrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.shopall.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopallFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopallFrontEndApplication.class, args);
+	}
+
+}
---
+++ ShopallWebParent/ShopallFrontEnd/src/test/java/com/shopall/ShopallFrontEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopallFrontEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
