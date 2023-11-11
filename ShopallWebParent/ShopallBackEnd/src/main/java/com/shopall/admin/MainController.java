59236e9e4a4ea9123277848661791cf6607a6f87
davidagina <davidagina64@gmail.com>
2023-11-11 02:12:28 +0100

    Updated BackEnd Controller

--- ShopallWebParent/ShopallBackEnd/src/main/java/com/shopall/admin/MainController.java
+++ ShopallWebParent/ShopallBackEnd/src/main/java/com/shopall/admin/MainController.java
@@ -1,5 +1,14 @@
 package com.shopall.admin;
 
-public class MainController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class MainController {
+	
+	@GetMapping("")
+	public String viewHomePage() {
+		return "index";
+	}
+	
 }
