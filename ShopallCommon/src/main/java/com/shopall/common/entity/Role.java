a9dc9bbd3338806d70da5948968bae8a9e0e415e
davidagina <davidagina64@gmail.com>
2023-11-18 19:54:00 +0100

    Added Role entity

--- ShopallCommon/src/main/java/com/shopall/common/entity/Role.java
+++ ShopallCommon/src/main/java/com/shopall/common/entity/Role.java
@@ -1,14 +1,24 @@
 package com.shopall.common.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "roles")
 public class Role {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	@Column(length = 40, nullable = false, unique = true)
 	private String name;
+	
+	@Column(length = 150, nullable = false)
 	private String description;
 	
 	
