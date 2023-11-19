a31fec990f11bb01a36023adf264f048825a950a
davidagina <davidagina64@gmail.com>
2023-11-19 03:06:13 +0100

    Added @EntityScan to scan .common and .admin package

--- ShopallWebParent/ShopallBackEnd/src/main/java/com/shopall/admin/ShopallBackEndApplication.java
+++ ShopallWebParent/ShopallBackEnd/src/main/java/com/shopall/admin/ShopallBackEndApplication.java
@@ -2,8 +2,10 @@ package com.shopall.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan({"com.shopall.common.entity", "com.shopall.admin.user"})
 public class ShopallBackEndApplication {
 
 	public static void main(String[] args) {
