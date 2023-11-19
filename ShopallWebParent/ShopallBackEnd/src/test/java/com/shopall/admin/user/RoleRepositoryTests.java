1bca816494d243534cb104ff6a3b3e48c9cf08ff
davidagina <davidagina64@gmail.com>
2023-11-19 02:14:39 +0100

    Added RoleRepositoryTests

--- ShopallWebParent/ShopallBackEnd/src/test/java/com/shopall/admin/user/RoleRepositoryTests.java
+++ ShopallWebParent/ShopallBackEnd/src/test/java/com/shopall/admin/user/RoleRepositoryTests.java
@@ -1,5 +1,23 @@
 package com.shopall.admin.user;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.shopall.common.entity.Role;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
 public class RoleRepositoryTests {
+	
+	@Autowired
+	private RoleRepository repo;
+	
+	@Test
+	public void testCreateFirstRole() {
+		Role roleAdmin = new Role("Admin", "Manage everything");
+	}
 
 }
