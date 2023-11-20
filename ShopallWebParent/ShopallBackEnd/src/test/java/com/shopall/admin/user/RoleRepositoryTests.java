3a79641a0a6522c8733c5a4090a3fe318a7f40c6
davidagina <davidagina64@gmail.com>
2023-11-20 04:30:45 +0100

    Updated

--- ShopallWebParent/ShopallBackEnd/src/test/java/com/shopall/admin/user/RoleRepositoryTests.java
+++ ShopallWebParent/ShopallBackEnd/src/test/java/com/shopall/admin/user/RoleRepositoryTests.java
@@ -7,11 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
 
 import com.shopall.common.entity.Role;
-
+ 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
 public class RoleRepositoryTests {
 	
 	@Autowired
