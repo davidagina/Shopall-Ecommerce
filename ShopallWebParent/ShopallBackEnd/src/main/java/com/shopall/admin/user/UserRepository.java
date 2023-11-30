3bcc04a976ba69df438b8a452e56337c6efb52fb
davidagina <davidagina64@gmail.com>
2023-11-30 14:59:15 +0100

    Added UserRepository class

---
+++ ShopallWebParent/ShopallBackEnd/src/main/java/com/shopall/admin/user/UserRepository.java
@@ -0,0 +1,9 @@
+package com.shopall.admin.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.shopall.common.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Integer>{
+	
+}
