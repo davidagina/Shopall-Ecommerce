db0c9ee138fccf6b930171a396cda0f31bac862e
davidagina <davidagina64@gmail.com>
2023-12-01 08:27:56 +0100

    Added createNewUserWithTwoRoles test

--- ShopallWebParent/ShopallBackEnd/src/test/java/com/shopall/admin/user/UserRepositoryTests.java
+++ ShopallWebParent/ShopallBackEnd/src/test/java/com/shopall/admin/user/UserRepositoryTests.java
@@ -1,11 +1,17 @@
 package com.shopall.admin.user;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.annotation.Rollback;
+
+import com.shopall.common.entity.Role;
+import com.shopall.common.entity.User;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = Replace.NONE)
@@ -15,8 +21,30 @@ public class UserRepositoryTests {
 	@Autowired
 	private UserRepository repo;
 	
+	@Autowired
+	private TestEntityManager entityManager;
+	
 	@Test
-	public void testCreateUser() {
+	public void testCreateNewUserWithOneRole() {
+		Role roleAdmin = entityManager.find(Role.class, 1);
+		User user = new User("Kingdaav@gmail.com", "abcd123", "King", "Daav");
+		user.addRole(roleAdmin);
 		
+		User savedUser = repo.save(user);
+		assertThat(savedUser.getId()).isGreaterThan(0);
+	}
+	
+	@Test
+	public void testCreateNewUserWithTwoRoles() {
+		User dabby = new User("dabby@gmail.com", "abc123", "Dabby", "Tam");
+		Role roleEditor = new Role(3);
+		Role roleAssistant = new Role(5);
+		
+		dabby.addRole(roleEditor);
+		dabby.addRole(roleAssistant);
+		
+		User savedUser = repo.save(dabby);
+		
+		assertThat(savedUser.getId()).isGreaterThan(0);
 	}
 }
