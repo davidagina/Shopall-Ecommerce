442bc0e4a0284078f2062c8c787523384773e78a
davidagina <davidagina64@gmail.com>
2023-12-01 08:49:24 +0100

    Added test all users method

--- ShopallWebParent/ShopallBackEnd/src/test/java/com/shopall/admin/user/UserRepositoryTests.java
+++ ShopallWebParent/ShopallBackEnd/src/test/java/com/shopall/admin/user/UserRepositoryTests.java
@@ -47,4 +47,12 @@ public class UserRepositoryTests {
 		
 		assertThat(savedUser.getId()).isGreaterThan(0);
 	}
+	
+	@Test
+	public void testListAllUsers() {
+		Iterable<User> listUsers = repo.findAll();
+		listUsers.forEach(user -> System.out.println(user));
+		
+		
+	}
 }
