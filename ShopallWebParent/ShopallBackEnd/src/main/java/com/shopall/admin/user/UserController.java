179a526a482fb701249ac58b02cc6002c5602e0e
davidagina <davidagina64@gmail.com>
2023-12-04 08:56:35 +0100

    Added new user controller

--- ShopallWebParent/ShopallBackEnd/src/main/java/com/shopall/admin/user/UserController.java
+++ ShopallWebParent/ShopallBackEnd/src/main/java/com/shopall/admin/user/UserController.java
@@ -21,4 +21,9 @@ public class UserController {
 		model.addAttribute("listUsers", listUsers);
 		return "users";
 	}
+	
+	@GetMapping("users/new")
+	public String newUser() {
+		return "user_form";
+	}
 }
