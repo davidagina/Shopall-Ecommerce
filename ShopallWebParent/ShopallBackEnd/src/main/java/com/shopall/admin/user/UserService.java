dfe8f01829c4e63f9c830b876b495cbb0f763fe9
davidagina <davidagina64@gmail.com>
2023-12-04 10:59:02 +0100

    Added listRoles method

--- ShopallWebParent/ShopallBackEnd/src/main/java/com/shopall/admin/user/UserService.java
+++ ShopallWebParent/ShopallBackEnd/src/main/java/com/shopall/admin/user/UserService.java
@@ -5,15 +5,22 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.shopall.common.entity.Role;
 import com.shopall.common.entity.User;
 
 @Service
 public class UserService {
 
 	@Autowired
-	private UserRepository repo;
+	private UserRepository userRepo;
+	
+	private RoleRepository roleRepo;
 	
 	public List<User> usersList(){
-		return repo.findAll();
+		return userRepo.findAll();
+	}
+	
+	public List<Role> listRoles() {
+		return (List<Role>) roleRepo.findAll();
 	}
 }
