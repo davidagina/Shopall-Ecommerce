857ddfc512b864a61cea002e6f4c81d45d157547
davidagina <davidagina64@gmail.com>
2023-12-06 18:01:31 +0100

    Refactored package name

---
+++ ShopallWebParent/ShopallBackEnd/src/main/java/com/shopall/admin/security/ShopallUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.shopall.admin.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.shopall.admin.user.UserRepository;
+import com.shopall.common.entity.User;
+
+public class ShopallUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	private UserRepository userRepo;
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		User user = userRepo.getUserByEmail(email);
+		if (user != null) {
+			return new ShopallUserDetails(user);
+		}
+		
+		throw new UsernameNotFoundException("Could not find user with email: " + email);
+	}
+
+}
