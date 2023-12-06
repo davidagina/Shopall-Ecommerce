97b7cd4fc3498e06b3fa7106786f0f5094c22c5a
davidagina <davidagina64@gmail.com>
2023-12-07 00:32:26 +0100

    Updated

--- ShopallWebParent/ShopallBackEnd/src/main/java/com/shopall/admin/user/UserRepository.java
+++ ShopallWebParent/ShopallBackEnd/src/main/java/com/shopall/admin/user/UserRepository.java
@@ -5,5 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.shopall.common.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Integer>{
+
+	User getUserByEmail(String email);
+	
 	
 }
