84b1faefdccf9be2fa599fe02ec065f39575769d
davidagina <davidagina64@gmail.com>
2023-12-08 14:40:56 +0100

    Added get user by email method

--- ShopallWebParent/ShopallBackEnd/src/main/java/com/shopall/admin/user/UserRepository.java
+++ ShopallWebParent/ShopallBackEnd/src/main/java/com/shopall/admin/user/UserRepository.java
@@ -1,12 +1,15 @@
 package com.shopall.admin.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.shopall.common.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Integer>{
 
-	User getUserByEmail(String email);
+	@Query("SELECT u FROM User u WHERE u.email = :email")
+	User getUserByEmail(@Param("email") String email);
 	
 	
 }
