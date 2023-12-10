85d5d1951387de1309e8699ebfb796623ccd9fc0
davidagina <davidagina64@gmail.com>
2023-12-10 03:33:45 +0100

    Added editUser handler method

--- ShopallWebParent/ShopallBackEnd/src/main/java/com/shopall/admin/user/UserController.java
+++ ShopallWebParent/ShopallBackEnd/src/main/java/com/shopall/admin/user/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -43,5 +44,14 @@ public class UserController {
 		
 		redirectAttributes.addFlashAttribute("message", "The user has been saved successfully.");
 		return "redirect:/users";
+	}
+	
+	
+	@GetMapping("/users/edit/{id}")
+	public String editUser(@PathVariable Integer id, Model model) {
+		User userProfile = service.getUserById(id);
+		model.addAttribute("userProfile", userProfile);
+		return "user_profile";
+	
 	}
 }
