d5d06222376102f385fcd591cbc4f73e920a2419
davidagina <davidagina64@gmail.com>
2023-12-13 15:04:04 +0100

    Added updateUserEnabledStatus

--- ShopallWebParent/ShopallBackEnd/src/main/java/com/shopall/admin/user/UserRepository.java
+++ ShopallWebParent/ShopallBackEnd/src/main/java/com/shopall/admin/user/UserRepository.java
@@ -1,6 +1,7 @@
 package com.shopall.admin.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -12,4 +13,9 @@ public interface UserRepository extends JpaRepository<User, Integer>{
 	User getUserByEmail(@Param("email") String email);
 	
 	public Long countById(Integer id);
+	
+	@Query("UPDATE User u SET u.enabled = ?2 WHERE u.id = ?1")
+	@Modifying
+	public void updateUserEnabledStatus(Integer id, boolean enabled);
+	
 }
