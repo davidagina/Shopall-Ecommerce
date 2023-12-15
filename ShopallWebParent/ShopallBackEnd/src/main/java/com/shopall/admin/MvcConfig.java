6f940d5adf1c64712982f488c4fef107396390d7
davidagina <davidagina64@gmail.com>
2023-12-15 18:59:42 +0100

    Added MvcConfig class

---
+++ ShopallWebParent/ShopallBackEnd/src/main/java/com/shopall/admin/MvcConfig.java
@@ -0,0 +1,16 @@
+package com.shopall.admin;
+
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class MvcConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		// TODO Auto-generated method stub
+		WebMvcConfigurer.super.addResourceHandlers(registry);
+	}
+	
+	
+
+}
