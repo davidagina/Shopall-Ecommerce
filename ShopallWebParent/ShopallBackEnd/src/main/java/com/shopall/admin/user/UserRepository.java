cd60f12f61c41817464f869e74b8a9ddc907425f
davidagina <davidagina64@gmail.com>
2023-12-20 22:10:11 +0100

    Added findAll method for search functionality

--- ShopallWebParent/ShopallBackEnd/src/main/java/com/shopall/admin/user/UserRepository.java
+++ ShopallWebParent/ShopallBackEnd/src/main/java/com/shopall/admin/user/UserRepository.java
@@ -1,5 +1,7 @@
 package com.shopall.admin.user;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -15,8 +17,12 @@ public interface UserRepository extends JpaRepository<User, Integer>, PagingAndS
 	
 	public Long countById(Integer id);
 	
+	@Query("SELECT u FROM User u WHERE u.firstName LIKE %?1% OR u.lastName LIKE %?1%")
+	public Page<User> findAll(String keyword, Pageable pageable);
+	
 	@Query("UPDATE User u SET u.enabled = ?2 WHERE u.id = ?1")
 	@Modifying
 	public void updateUserEnabledStatus(Integer id, boolean enabled);
+	
 	
 }
