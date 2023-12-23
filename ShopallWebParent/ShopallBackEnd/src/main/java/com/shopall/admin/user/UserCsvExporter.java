c64cfc3e8a3b8b04c0cfc4675af509071b67bf6c
davidagina <davidagina64@gmail.com>
2023-12-24 00:50:54 +0100

    Added UserCsvExporter class

---
+++ ShopallWebParent/ShopallBackEnd/src/main/java/com/shopall/admin/user/UserCsvExporter.java
@@ -0,0 +1,16 @@
+package com.shopall.admin.user;
+
+import java.util.List;
+
+import com.shopall.common.entity.User;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+public class UserCsvExporter {
+	
+	public void export(List<User> listUsers, HttpServletResponse response) {
+		
+		
+	}
+
+}
