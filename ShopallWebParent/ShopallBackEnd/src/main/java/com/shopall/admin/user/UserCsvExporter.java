44c830b6bb1ff5872be98f1bba33f0d528a47ed7
davidagina <davidagina64@gmail.com>
2023-12-24 21:19:40 +0100

    Complete UserCsvExporter class

--- ShopallWebParent/ShopallBackEnd/src/main/java/com/shopall/admin/user/UserCsvExporter.java
+++ ShopallWebParent/ShopallBackEnd/src/main/java/com/shopall/admin/user/UserCsvExporter.java
@@ -1,6 +1,14 @@
 package com.shopall.admin.user;
 
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+
+import org.supercsv.io.CsvBeanWriter;
+import org.supercsv.io.ICsvBeanWriter;
+import org.supercsv.prefs.CsvPreference;
 
 import com.shopall.common.entity.User;
 
@@ -8,7 +16,26 @@ import jakarta.servlet.http.HttpServletResponse;
 
 public class UserCsvExporter {
 	
-	public void export(List<User> listUsers, HttpServletResponse response) {
+	public void export(List<User> listUsers, HttpServletResponse response) throws IOException {
+		
+		DateFormat dateFormatter = new SimpleDateFormat("YYYY-MM-dd_HH-mm-ss");
+		String timeStamp = dateFormatter.format(new Date());
+		String fileName = "users_" + timeStamp + ".csv";
+		
+		response.setContentType("text/csv");
+		
+		String headerKey = "Content-Disposition";
+		String headerValue = "attachment; filename=" + fileName;
+		response.setHeader(headerKey, headerValue);
+		
+		ICsvBeanWriter csvWriter = new CsvBeanWriter(response.getWriter(), 
+				CsvPreference.STANDARD_PREFERENCE);
+		
+		String[]  csvHeader = {"User ID", "E-Mail", "First Name", "Last Name", "Roles", "Enabled"};
+		
+		csvWriter.writeHeader(csvHeader);
+		
+		csvWriter.close();
 		
 		
 	}
