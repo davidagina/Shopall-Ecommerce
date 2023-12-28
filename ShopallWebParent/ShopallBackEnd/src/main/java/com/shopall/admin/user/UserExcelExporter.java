3214230223d15768c705f5e90f41631c0001e8dd
davidagina <davidagina64@gmail.com>
2023-12-28 22:23:12 +0100

    Updated

--- ShopallWebParent/ShopallBackEnd/src/main/java/com/shopall/admin/user/UserExcelExporter.java
+++ ShopallWebParent/ShopallBackEnd/src/main/java/com/shopall/admin/user/UserExcelExporter.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.util.List;
 
 import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFFont;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -15,15 +17,28 @@ import jakarta.servlet.http.HttpServletResponse;
 
 public class UserExcelExporter extends AbstractExporter{
 	
+	private XSSFWorkbook workbook;
+	private XSSFSheet sheet;
+	
 	public void export(List<User> listUsers, HttpServletResponse response) throws IOException {
 			
-			super.setResponseHeader(response, "application/octet-stream", ".xslx");
+			super.setResponseHeader(response, "application/octet-stream", ".xlsx");
 			
 			XSSFWorkbook workbook = new XSSFWorkbook();
 			XSSFSheet sheet = workbook.createSheet("Users");
 			XSSFRow row = sheet.createRow(0);
+			
+			XSSFCellStyle cellStyle = workbook.createCellStyle();
+			XSSFFont font = workbook.createFont();
+			font.setBold(true);
+			font.setFontHeight(16);
+			cellStyle.setFont(font);
+			
 			XSSFCell cell = row.createCell(0);
 			cell.setCellValue("User ID");
+			cell.setCellStyle(cellStyle);
+			
+			
 			
 			ServletOutputStream outputStream = response.getOutputStream();
 			workbook.write(outputStream);
