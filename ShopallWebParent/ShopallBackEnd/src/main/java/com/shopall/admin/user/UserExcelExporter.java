f3207fb9967452abd12e8efb7334ebb7653f913c
davidagina <davidagina64@gmail.com>
2023-12-28 22:57:35 +0100

    Implemented write headers to csv

--- ShopallWebParent/ShopallBackEnd/src/main/java/com/shopall/admin/user/UserExcelExporter.java
+++ ShopallWebParent/ShopallBackEnd/src/main/java/com/shopall/admin/user/UserExcelExporter.java
@@ -3,6 +3,7 @@ package com.shopall.admin.user;
 import java.io.IOException;
 import java.util.List;
 
+import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFCellStyle;
 import org.apache.poi.xssf.usermodel.XSSFFont;
@@ -20,25 +21,47 @@ public class UserExcelExporter extends AbstractExporter{
 	private XSSFWorkbook workbook;
 	private XSSFSheet sheet;
 	
+	public UserExcelExporter() {
+		workbook = new XSSFWorkbook();
+	}
+	
+	private void writeHeaderLine() {
+		sheet = workbook.createSheet("Users");
+		XSSFRow row = sheet.createRow(0);
+		
+		XSSFCellStyle cellStyle = workbook.createCellStyle();
+		XSSFFont font = workbook.createFont();
+		font.setBold(true);
+		font.setFontHeight(16);
+		cellStyle.setFont(font);
+		
+		createCell(row, 0, "User Id", cellStyle);
+		createCell(row, 1, "E-mail", cellStyle);
+		createCell(row, 2, "First Name", cellStyle);
+		createCell(row, 3, "Last Name", cellStyle);
+		createCell(row, 4, "Roles", cellStyle);
+		createCell(row, 5, "Enabled", cellStyle);
+	}
+	
+	private void createCell(XSSFRow row, int columnIndex, Object value, CellStyle style) {
+		XSSFCell cell = row.createCell(columnIndex);
+		
+		if (value instanceof Integer) {
+			cell.setCellValue((Integer) value);
+		} else if (value instanceof Boolean) {
+			cell.setCellValue((Integer) value);
+		} else {
+			cell.setCellValue((String) value);
+		}
+		
+		cell.setCellStyle(style);
+	}
+	
 	public void export(List<User> listUsers, HttpServletResponse response) throws IOException {
 			
 			super.setResponseHeader(response, "application/octet-stream", ".xlsx");
-			
-			XSSFWorkbook workbook = new XSSFWorkbook();
-			XSSFSheet sheet = workbook.createSheet("Users");
-			XSSFRow row = sheet.createRow(0);
-			
-			XSSFCellStyle cellStyle = workbook.createCellStyle();
-			XSSFFont font = workbook.createFont();
-			font.setBold(true);
-			font.setFontHeight(16);
-			cellStyle.setFont(font);
-			
-			XSSFCell cell = row.createCell(0);
-			cell.setCellValue("User ID");
-			cell.setCellStyle(cellStyle);
-			
-			
+		
+			writeHeaderLine();
 			
 			ServletOutputStream outputStream = response.getOutputStream();
 			workbook.write(outputStream);
