786cf08d5c8f1c8737c9a591bfb78ce9bf4c6929
davidagina <davidagina64@gmail.com>
2023-12-29 07:16:20 +0100

    Created UserPdfExporter class

---
+++ ShopallWebParent/ShopallBackEnd/src/main/java/com/shopall/admin/user/UserPdfExporter.java
@@ -0,0 +1,16 @@
+package com.shopall.admin.user;
+
+import java.util.List;
+
+import com.shopall.common.entity.User;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+public class UserPdfExporter extends AbstractExporter{
+
+	public void export(List<User> listAll, HttpServletResponse response) {
+		
+		
+	}
+
+}
