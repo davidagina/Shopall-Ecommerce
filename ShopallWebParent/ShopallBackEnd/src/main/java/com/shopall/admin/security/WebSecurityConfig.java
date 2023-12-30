3999f3e1389fe7d693aed1cb08db5a2e5754024e
davidagina <davidagina64@gmail.com>
2023-12-30 15:45:58 +0100

    Updated to use custom login form

--- ShopallWebParent/ShopallBackEnd/src/main/java/com/shopall/admin/security/WebSecurityConfig.java
+++ ShopallWebParent/ShopallBackEnd/src/main/java/com/shopall/admin/security/WebSecurityConfig.java
@@ -1,10 +1,15 @@
 package com.shopall.admin.security;
 
+
 import org.springframework.context.annotation.Bean;
-import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configurers.userdetails.DaoAuthenticationConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,18 +19,43 @@ import org.springframework.security.web.SecurityFilterChain;
 public class WebSecurityConfig {
 	
 	@Bean
+	UserDetailsService userDetailsService() {
+		return new ShopallUserDetailsService();
+	}
+	
+	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+		authProvider.setUserDetailsService(userDetailsService());
+		authProvider.setPasswordEncoder(passwordEncoder());
+		
+		return authProvider;
 	}
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-            .authorizeHttpRequests((authz) -> authz
-                .anyRequest().permitAll()
-            ).httpBasic(withDefaults());
-            
-        return http.build();
-    }
+    	http.authenticationProvider(authenticationProvider());
+			http
+				.authorizeHttpRequests((auth) -> auth
+						.anyRequest().authenticated()
+						).formLogin(form -> form
+								.loginPage("/login")
+								.usernameParameter("email")
+								.permitAll());
+			
+		return http.build();
 
+    }
+    
+   @Bean
+   public WebSecurityCustomizer webSecurityCustomizer() {
+	   return(web) -> web.ignoring().requestMatchers("/images/**", "/js/**", "/webjars/**");
+   }
+   
+   
 }
