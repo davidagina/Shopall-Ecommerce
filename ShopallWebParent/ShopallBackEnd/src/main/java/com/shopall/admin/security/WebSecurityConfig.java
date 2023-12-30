91d06a42567ce47084623b02a3a030988b8d91d9
davidagina <davidagina64@gmail.com>
2023-12-30 19:50:05 +0100

    Implemented methods to enable login functionality

--- ShopallWebParent/ShopallBackEnd/src/main/java/com/shopall/admin/security/WebSecurityConfig.java
+++ ShopallWebParent/ShopallBackEnd/src/main/java/com/shopall/admin/security/WebSecurityConfig.java
@@ -4,9 +4,8 @@ package com.shopall.admin.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.configurers.userdetails.DaoAuthenticationConfigurer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -35,6 +34,10 @@ public class WebSecurityConfig {
 		authProvider.setPasswordEncoder(passwordEncoder());
 		
 		return authProvider;
+	}
+	
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.authenticationProvider(authenticationProvider());
 	}
 
     @Bean
