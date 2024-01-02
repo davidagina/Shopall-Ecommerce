a669a41df45c866f0433b070b77efaf717fe9854
davidagina <davidagina64@gmail.com>
2024-01-02 11:49:20 +0100

    Implemented update account to reflect name when changed

--- ShopallWebParent/ShopallBackEnd/src/main/java/com/shopall/admin/user/AccountController.java
+++ ShopallWebParent/ShopallBackEnd/src/main/java/com/shopall/admin/user/AccountController.java
@@ -1,11 +1,19 @@
 package com.shopall.admin.user;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.shopall.admin.FileUploadUtil;
 import com.shopall.admin.security.ShopallUserDetails;
 import com.shopall.common.entity.User;
 
@@ -23,5 +31,34 @@ public class AccountController {
 		model.addAttribute("user", user);
 		
 		return "account_form";
+	}
+	
+	
+	@PostMapping("/users/update")
+	public String saveDetails(User user, RedirectAttributes redirectAttributes,
+			@AuthenticationPrincipal ShopallUserDetails loggedUser,
+			@RequestParam("image") MultipartFile multipartFile) throws IOException {
+
+		if (!multipartFile.isEmpty()) {
+			String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+			user.setPhotos(fileName);
+			
+			User savedUser = service.updateAccount(user);
+			
+			String uploadDir = "user-photos/" + savedUser.getId();
+		
+			FileUploadUtil.cleanDir(uploadDir);
+			FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
+		} else {
+			if (user.getPhotos().isEmpty())
+				user.setPhotos(null);
+			service.updateAccount(user);
+		}
+		
+		loggedUser.setFirstName(user.getFirstName());
+		loggedUser.setLastName(user.getLastName());
+		
+		redirectAttributes.addFlashAttribute("message", "Your accounts have been updated.");
+		return "redirect:/account";
 	}
 }
