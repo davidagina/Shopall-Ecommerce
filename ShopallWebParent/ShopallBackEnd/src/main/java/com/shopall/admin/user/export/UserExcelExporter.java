0612f49cbb12f5cee1e5258b5efb821912ca8ca2
davidagina <davidagina64@gmail.com>
2024-01-02 21:27:27 +0100

    Moved to one package

--- ShopallWebParent/ShopallBackEnd/src/main/java/com/shopall/admin/user/export/UserExcelExporter.java
+++ ShopallWebParent/ShopallBackEnd/src/main/java/com/shopall/admin/user/export/UserExcelExporter.java
@@ -11,7 +11,6 @@ import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import com.shopall.admin.user.AbstractExporter;
 import com.shopall.common.entity.User;
 
 import jakarta.servlet.ServletOutputStream;
--- ShopallWebParent/ShopallBackEnd/src/main/java/com/shopall/admin/user/export/UserPdfExporter.java
+++ ShopallWebParent/ShopallBackEnd/src/main/java/com/shopall/admin/user/export/UserPdfExporter.java
@@ -13,7 +13,6 @@ import com.lowagie.text.Phrase;
 import com.lowagie.text.pdf.PdfPCell;
 import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfWriter;
-import com.shopall.admin.user.AbstractExporter;
 import com.shopall.common.entity.User;
 
 import jakarta.servlet.http.HttpServletResponse;
