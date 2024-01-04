7984520319209fc651f19cb32bb7a7f9a8dafcdb
davidagina <davidagina64@gmail.com>
2024-01-04 23:10:58 +0100

    Removed onclick function on main link

--- ShopallWebParent/ShopallBackEnd/src/main/resources/static/js/common.js
+++ ShopallWebParent/ShopallBackEnd/src/main/resources/static/js/common.js
@@ -16,7 +16,7 @@ function customizerDropDownMenu() {
 			$(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp();
 		}
 	);
-	$(".dropdown > a").click(function() {
+	/* $(".dropdown > a").click(function() {
 		location.href = this.href;
-	});
+	}); */
 }
