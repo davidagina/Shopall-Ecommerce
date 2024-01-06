5cfce6fa4835583d9216cc43e503152fd3ff79c4
davidagina <davidagina64@gmail.com>
2024-01-06 19:04:50 +0100

    Added findRootCategories method

--- ShopallWebParent/ShopallBackEnd/src/main/java/com/shopall/admin/category/CategoryRepository.java
+++ ShopallWebParent/ShopallBackEnd/src/main/java/com/shopall/admin/category/CategoryRepository.java
@@ -1,11 +1,15 @@
 package com.shopall.admin.category;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.shopall.common.entity.Category;
 
 public interface CategoryRepository extends JpaRepository<Category, Integer>, PagingAndSortingRepository<Category, Integer>{
 
-	
+	@Query("SELECT c FROM Category c WHERE c.parent.id is NULL")
+	public List<Category> findRootCategories();
 }
