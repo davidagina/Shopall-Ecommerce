2f7f73048b40cbc33836a772feef084ee555dcf9
davidagina <davidagina64@gmail.com>
2024-01-07 15:42:33 +0100

    Added copyFull and copyIdAndName method

--- ShopallCommon/src/main/java/com/shopall/common/entity/Category.java
+++ ShopallCommon/src/main/java/com/shopall/common/entity/Category.java
@@ -65,6 +65,26 @@ public class Category {
 	}
 	
 	
+	public static Category copyFull(Category category) {
+		Category copyCategory = new Category();
+		copyCategory.setId(category.getId());
+		copyCategory.setName(category.getName());
+		copyCategory.setImage(category.getImage());
+		copyCategory.setAlias(category.getAlias());
+		copyCategory.setEnabled(category.isEnabled());
+		
+		return copyCategory;
+	}
+	
+	
+	public static Category copyFull(Category category, String name) {
+		Category copyCategory = Category.copyFull(category);
+		copyCategory.setName(name);
+		
+		return copyCategory;
+	}
+	
+	
 	public Category(String name) {
 		this.name = name;
 		this.alias = name;
