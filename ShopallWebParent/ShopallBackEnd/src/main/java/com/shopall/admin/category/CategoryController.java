1d0b1cdf498f7fd128278eccbb8cd42393eeaaf0
davidagina <davidagina64@gmail.com>
2024-01-07 16:50:47 +0100

    Added save categories handler method

--- ShopallWebParent/ShopallBackEnd/src/main/java/com/shopall/admin/category/CategoryController.java
+++ ShopallWebParent/ShopallBackEnd/src/main/java/com/shopall/admin/category/CategoryController.java
@@ -1,13 +1,21 @@
 package com.shopall.admin.category;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.shopall.admin.FileUploadUtil;
 import com.shopall.common.entity.Category;
+
 
 @Controller
 public class CategoryController {
@@ -30,6 +38,21 @@ public class CategoryController {
 		model.addAttribute("listCategories", listCategories);
 		model.addAttribute("pageTitle", "Create New Category");
 		return "category/category_form";
+	}
+	
+	@PostMapping("/categories/save")
+	public String saveCategory(Category category, @RequestParam("fileImage") 
+	MultipartFile multipartFile, RedirectAttributes redirectAttributes) throws IOException {
+		
+		String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+		category.setImage(fileName);
+		
+		Category savedCategory = service.save(category);
+		String uploadDir = "../category-images/" + savedCategory.getId();
+		FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
+		
+		redirectAttributes.addFlashAttribute("message", "The category has been saved successfully.");
+		return "redirect:/categories";
 	}
 
 }
