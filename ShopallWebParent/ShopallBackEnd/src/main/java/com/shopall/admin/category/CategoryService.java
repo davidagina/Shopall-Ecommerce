3a1ded4f75c0fe86a12a90d216aba1213d04b2dc
davidagina <davidagina64@gmail.com>
2024-01-08 01:35:04 +0100

    Added getCategoryById and listByPage methods

--- ShopallWebParent/ShopallBackEnd/src/main/java/com/shopall/admin/category/CategoryService.java
+++ ShopallWebParent/ShopallBackEnd/src/main/java/com/shopall/admin/category/CategoryService.java
@@ -2,9 +2,14 @@ package com.shopall.admin.category;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.shopall.common.entity.Category;
@@ -12,13 +17,34 @@ import com.shopall.common.entity.Category;
 @Service
 public class CategoryService {
 	
+	public static final int CATEGORIES_PER_PAGE = 4;
+	
 	@Autowired
 	private CategoryRepository repo;
+	
+	
 	
 	public List<Category> listAll(){
 		List<Category> rootCategories = repo.findRootCategories();
 		return listHierarchicalCategories(rootCategories);
 	}
+	
+	
+	
+	public Page<Category> listByPage(int pageNum, String sortField, String sortDir, String keyword){
+		Sort sort = Sort.by(sortField);
+		
+		sort = sortDir.equals("asc") ? sort.ascending() : sort.descending();
+		Pageable pageable = PageRequest.of(pageNum - 1, CATEGORIES_PER_PAGE, sort);
+		
+		if (keyword != null) {
+			return repo.findAll(keyword, pageable);
+		}
+		
+		return repo.findAll(pageable);
+	}
+	
+	
 	
 	public List<Category> listHierarchicalCategories(List<Category> rootCategories){
 		List<Category> hierarchicalCategories = new ArrayList<>();
@@ -100,6 +126,17 @@ public class CategoryService {
 			
 			listSubCategoriesUsedInForm(categoriesUsedInForm, subCategory, newSubLevel);
 		}
+	}
+	
+	
+	public Category getCategoryById(Integer id) throws CategoryNotFoundException {
+		try {
+			return repo.findById(id).get();
+		} catch (NoSuchElementException ex) {
+			// TODO: handle exception
+			throw new CategoryNotFoundException("Could not find any category with ID " + id);
+		}
+		
 	}
 
 }
