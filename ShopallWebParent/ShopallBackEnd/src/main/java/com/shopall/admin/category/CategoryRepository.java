92b921b032c75162c013027c6d53321f31929c90
davidagina <davidagina64@gmail.com>
2024-01-08 01:42:17 +0100

    Added findAll method

--- ShopallWebParent/ShopallBackEnd/src/main/java/com/shopall/admin/category/CategoryRepository.java
+++ ShopallWebParent/ShopallBackEnd/src/main/java/com/shopall/admin/category/CategoryRepository.java
@@ -2,6 +2,8 @@ package com.shopall.admin.category;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
@@ -12,4 +14,8 @@ public interface CategoryRepository extends JpaRepository<Category, Integer>, Pa
 
 	@Query("SELECT c FROM Category c WHERE c.parent.id is NULL")
 	public List<Category> findRootCategories();
+	
+	@Query("SELECT c FROM Category c WHERE CONCAT(c.id, ' ', c.name, ' ',"
+			+ " c.alias) LIKE %?1%")
+	public Page<Category> findAll(String keyword, Pageable pageable);
 }
